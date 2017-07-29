c6dc65ee7fdd4ab9ac8e6161648e9bbebfd52228
Eugene Piven <thepegasd@gmail.com>
2017-07-29 23:06:28 +0300

    Add coveralls support

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,5 +1,7 @@
 require 'bundler/setup'
 require 'keepthechange'
+require 'coveralls'
+Coveralls.wear!
 
 RSpec.configure do |config|
   config.example_status_persistence_file_path = '.rspec_status'
